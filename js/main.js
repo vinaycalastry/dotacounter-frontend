6d02523bd6abd04fe941ee2b07b33fdc0122485a
Vinay <vinay.calastry@gmail.com>
2018-08-21 23:28:23 -0700

    splitting repos to front end and backend

---
+++ js/main.js
@@ -0,0 +1,35 @@
+new Vue({
+    el: '#dotaCounterApp',
+    data: {
+        badAgainstHeroes: [],
+        goodAgainstHeroes: [],
+        worksWellWithHeroes:[],
+        success:false,
+        nodata: false
+    },
+    methods:{
+        searchhero: function(){
+            axios.get('http://localhost:3000/dotacounter/'+this.$refs.heroname.value).then(
+                (response) => {              
+                    var heroes = response.data;    
+                    this.badAgainstHeroes = heroes["Bad Against"];
+                    this.goodAgainstHeroes = heroes["Good Against"];
+                    this.worksWellWithHeroes = heroes["Works Well With"];
+                    this.success = true;
+                    this.nodata = false;                         
+                }
+            ).catch(
+                (error)=>{
+                    if(error.response.status === 422){
+                        this.nodata = true;
+                        this.success = false;
+                    }
+                    else{
+                        this.nodata = false;
+                        this.success = false;
+                    }                    
+                }
+            );
+        }
+    }
+});
